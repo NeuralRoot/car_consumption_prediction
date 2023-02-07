a888fcbf05054fa60be2d29fbc8eeab158a82d53
Neural Root <pablocoboperez07@gmail.com>
2023-02-07 20:14:54 +0100

    commit

---
+++ .deploy/README.md
@@ -0,0 +1,2 @@
+# Fuel-Prediction-21-01-2023-
+ML linear regression model implemented on a web page
---
+++ .deploy/app.py
@@ -0,0 +1,31 @@
+import flask
+import pickle
+
+with open(f'model/linear_regression_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = flask.Flask(__name__, template_folder='templates')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('index.html'))
+
+    elif flask.request.method == 'POST':
+        acceleration = int(flask.request.form['acceleration'])
+        weight = int(flask.request.form['weight'])
+
+        input_variables = [[acceleration, weight]]
+
+        prediction = int(model.predict(input_variables)[0])
+
+        return flask.render_template('index.html',
+                                     original_input={'Acceleration': acceleration,
+                                                     'Weight': weight},
+                                     result=prediction,
+                                     )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80,debug=True)
